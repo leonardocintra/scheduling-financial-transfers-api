f1e20dd6fef003cba848850ac903cd290479bffb
leo <leonardo.ncintra@outlook.com>
2020-09-11 13:40:01 -0300

    created customer model

---
+++ src/main/java/com/leaolabs/scheduling/model/Customer.java
@@ -0,0 +1,31 @@
+package com.leaolabs.scheduling.model;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "customer")
+@Getter
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "created_at")
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private ZonedDateTime updatedAt;
+
+    @PrePersist
+    protected void prePersist() {
+        createdAt = updatedAt = ZonedDateTime.now();
+    }
+}
+
